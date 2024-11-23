dda0e8b977d48d4acc9138b10b47be9eb4606a8e
Nichita Ciuprin <nichitaciuprin@gmail.com>
2024-11-23 14:30:56 +0200

    UPDATE

--- tests/Empty/build.bat
+++ tests/Empty/build.bat
@@ -10,17 +10,17 @@ set flags=%flags% -g3 -Og -std=c17 -pthread -m64
 set flags=%flags% -Werror -Wall -Wno-missing-braces
 
 set src=
-set src=%src% %deps%\corelib\src\SysHelper.c
-set src=%src% %deps%\corelib\src\SysWindow.c
-set src=%src% %deps%\corelib\src\SysNet.c
+@REM set src=%src% %deps%\corelib\src\SysHelper.c
+@REM set src=%src% %deps%\corelib\src\SysWindow.c
+@REM set src=%src% %deps%\corelib\src\SysNet.c
 
 set include=
-set include=%include% -I%~dp0src
-set include=%include% -I%deps%\corelib\include
+@REM set include=%include% -I%~dp0src
+@REM set include=%include% -I%deps%\corelib\include
 
 set lib=
-set lib=%lib% -lgdi32
-set lib=%lib% -lWs2_32
+@REM set lib=%lib% -lgdi32
+@REM set lib=%lib% -lWs2_32
 
 if exist %build% rmdir /S /Q %build%
    mkdir %build%
