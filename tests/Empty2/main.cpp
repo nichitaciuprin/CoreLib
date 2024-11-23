c4fcdadacd4c35536760ed091de7245beec8c621
Nichita Ciuprin <nichitaciuprin@gmail.com>
2024-11-23 16:15:32 +0200

    Updated Empty2

--- tests/Empty2/main.cpp
+++ tests/Empty2/main.cpp
@@ -1,20 +1,4 @@
-// #include <stdio.h>
-// #include <stdlib.h>
-// #include <stdbool.h>
-// #include <string.h>
-// #include <memory.h>
-// #include <time.h>
-// #include <math.h>
-
-#include "Helper.h"
-
 int main()
 {
-    // Vector3 v = { 1.02f, 0.23, 12.11111f };
-
-    PrintFloat(1.02f);
-
-    // PrintVector3(v);
-
     return 0;
 }
