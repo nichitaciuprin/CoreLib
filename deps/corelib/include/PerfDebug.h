b576de6c8d079f61b3440f1c6765f74f6141f56b
Nichita Ciuprin <nichitaciuprin@gmail.com>
2024-11-26 18:34:10 +0200

    Added PerfDebug

---
+++ deps/corelib/include/PerfDebug.h
@@ -0,0 +1,30 @@
+#pragma once
+
+#include "stdio.h"
+#include "SysHelper.h"
+
+int PrintAvgPerf_TimeStart = 0;
+int PrintAvgPerf_TimeEnd = 0;
+int PrintAvgPerf_Count = 0;
+int PrintAvgPerf_CountMax = 100;
+int PrintAvgPerf_Time = 0;
+void PrintAvgPerf_Start()
+{
+    PrintAvgPerf_TimeStart = GetTime();
+}
+void PrintAvgPerf_End()
+{
+    PrintAvgPerf_TimeEnd = GetTime();
+
+    PrintAvgPerf_Count++;
+    PrintAvgPerf_Time += PrintAvgPerf_TimeEnd - PrintAvgPerf_TimeStart;
+
+    if (PrintAvgPerf_Count != PrintAvgPerf_CountMax) return;
+
+    int result = PrintAvgPerf_Time /= PrintAvgPerf_CountMax;
+
+    PrintAvgPerf_Count = 0;
+    PrintAvgPerf_Time = 0;
+
+    printf("PrintAvgPerf: %i\n", result);
+}
