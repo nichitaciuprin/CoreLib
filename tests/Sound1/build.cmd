6489a162ae97e175e1683e1357d2a3412bd51752
Nichita Ciuprin <nichita.ciuprin@feel-it-services.com>
2024-12-11 17:34:42 +0200

    Moved miniaudio to its own directory

--- tests/Sound1/build.cmd
+++ tests/Sound1/build.cmd
@@ -2,18 +2,24 @@
 setlocal
 
 set deps=%~dp0..\..\deps
+set build=%~dp0build
 set main=%~dp0main.cpp
 
 set flags=
 set flags=%flags% -g3 -Og -std=c++17 -pthread -m64
 set flags=%flags% -Werror -Wall -Wno-missing-braces
 
+set src=
+
 set include=
-set include=%include% -I%deps%\corelib\include
+set include=%include% -I%~dp0src
+set include=%include% -I%deps%\MiniAudio\include
 
-if exist build rmdir /S /Q build
-   mkdir build
+set lib=
 
-g++ %main% -o build/main.exe %include% %flags%
+if exist %build% rmdir /S /Q %build%
+   mkdir %build%
+
+g++ %main% %src% -o build/main.exe %include% %lib% %flags%
 
 copy .\sound.wav .\build\sound.wav > nul
