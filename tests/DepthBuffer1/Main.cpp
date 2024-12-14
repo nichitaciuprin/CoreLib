119b535abefda6b8472a3ac09f5a7003033ed1e4
Nichita Ciuprin <nichitaciuprin@gmail.com>
2024-12-14 17:35:52 +0200

    Fixed DepthBuffer1

--- tests/DepthBuffer1/Main.cpp
+++ tests/DepthBuffer1/Main.cpp
@@ -1,47 +1,46 @@
-#include "BaseLang.hpp"
-#include "SysHelper.h"
-#include "BaseMath.h"
-#include "Bitmap.hpp"
 #include "SysWindow.hpp"
+#include "Bitmap.hpp"
+#include "BaseLang.hpp"
 
 int main()
 {
     auto width = 400;
     auto height = 400;
 
-    auto bitmap = make_unique<BitmapClass>(width, height);
-    auto window = make_unique<Window>(700, 100, width, height);
+    auto bitmap_ = BitmapCreate(width, height);
+    auto bitmap = &bitmap_;
+    auto window = SysWindowCreate(700, 100, width, height);
 
-    while (window->Exists())
+    while (SysWindowExists(window))
     {
-        bitmap->Reset();
+        BitmapReset(bitmap);
+
+        float zFar = 10;
+        float zClose = 9;
 
         {
-            float zFar = 10;
-            float zClose = 9;
-            {
-                Vector3 p0 = { -0.2f,  0.6f,  zFar };
-                Vector3 p1 = {  0.0f,  0.6f,  zFar };
-                Vector3 p2 = {  0.4f, -0.6f,  zClose };
-                bitmap->DrawTriangleNdc(p0, p1, p2, COLOR_RED);
-            }
-            {
-                Vector3 p0 = {  0.6f, -0.2f,  zFar };
-                Vector3 p1 = {  0.6f, -0.4f,  zFar };
-                Vector3 p2 = { -0.8f, -0.2f,  zClose };
-                bitmap->DrawTriangleNdc(p0, p1, p2, COLOR_GREEN);
-            }
-            {
-                Vector3 p0 = { -0.4f, -0.4f,  zFar };
-                Vector3 p1 = { -0.6f, -0.4f,  zFar };
-                Vector3 p2 = {  0.2f,  0.6f,  zClose };
-                bitmap->DrawTriangleNdc(p0, p1, p2, COLOR_BLUE);
-            }
+            Vector3 p0 = { -0.2f,  0.6f,  zFar };
+            Vector3 p1 = {  0.0f,  0.6f,  zFar };
+            Vector3 p2 = {  0.4f, -0.6f,  zClose };
+            BitmapDrawTriangleNdc(bitmap, p0, p1, p2, COLOR_RED);
+        }
+        {
+            Vector3 p0 = {  0.6f, -0.2f,  zFar };
+            Vector3 p1 = {  0.6f, -0.4f,  zFar };
+            Vector3 p2 = { -0.8f, -0.2f,  zClose };
+            BitmapDrawTriangleNdc(bitmap, p0, p1, p2, COLOR_GREEN);
+        }
+        {
+            Vector3 p0 = { -0.4f, -0.4f,  zFar };
+            Vector3 p1 = { -0.6f, -0.4f,  zFar };
+            Vector3 p2 = {  0.2f,  0.6f,  zClose };
+            BitmapDrawTriangleNdc(bitmap, p0, p1, p2, COLOR_BLUE);
         }
 
-        bitmap->FillBorder(COLOR_GREEN);
-        window->SetPixels(bitmap->pixels, bitmap->Width(), bitmap->Height());
-        window->Update();
+        BitmapFillBorder(bitmap, COLOR_GREEN);
+
+        SysWindowSetPixels(window, bitmap->pixels, bitmap->width, bitmap->height);
+        SysWindowUpdate(window);
     }
 
     return 0;
